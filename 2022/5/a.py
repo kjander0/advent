048f08e2db6ac3f735d38e5d5c1a88982d5dba0a
Kieren Anderson <kjander0@gmail.com>
2022-12-05 17:38:04 +1000

    Day 5

---
+++ 2022/5/a.py
@@ -0,0 +1,50 @@
+infile = open('5/testInput', 'r')
+
+def read_row(line):
+    items = []
+    index = 1
+    while index < len(line):
+        item = line[index]
+        if item == ' ':
+            item = None
+        items.append(item)
+        index += 4
+    return items
+
+def read_stacks():
+    stacks = []
+    while True:
+        line = infile.readline()
+        if line[1] == '1': # finished reading stacks
+            break
+        row = read_row(line)
+        if len(stacks) == 0:
+            for i in range(len(row)):
+                stacks.append([])
+
+        for i in range(len(row)):
+            if row[i] is not None:
+                stacks[i].insert(0, row[i])
+    return stacks
+
+stacks = read_stacks()
+
+infile.readline() # read blank line
+
+while True:
+    line = infile.readline().strip()
+    if line == '': # EOF
+        break
+
+    pieces = line.split()
+    num, frm, to = int(pieces[1]), int(pieces[3]), int(pieces[5])
+    print(num, frm, to)
+
+    for i in range(num):
+        stacks[to-1].append(stacks[frm-1].pop())
+
+result = ''
+for stack in stacks:
+    result += stack[-1]
+
+print(result)
